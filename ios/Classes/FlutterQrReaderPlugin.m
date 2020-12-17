1d40b531378380f71ebe25326af9ba9a4200c250
tienth <tienth@hanbiro.vn>
2020-12-17 15:32:21 +0700

    Add scanner barcode ios

--- ios/Classes/FlutterQrReaderPlugin.m
+++ ios/Classes/FlutterQrReaderPlugin.m
@@ -1,5 +1,8 @@
 #import "FlutterQrReaderPlugin.h"
 #import "QrReaderViewController.h"
+#import <Vision/VNDetectBarcodesRequest.h>
+#import <Vision/VNRequestHandler.h>
+#import <Vision/VNObservation.h>
 
 @implementation FlutterQrReaderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -31,9 +34,32 @@
     if (features.count > 0) {
         CIQRCodeFeature *feature = [features objectAtIndex:0];
         NSString *qrData = feature.messageString;
+        NSLog(@"TEST message photo: %@", qrData);
         result(qrData);
     } else {
-        result(NULL);
+        if (@available(iOS 11.0, *)) {
+            VNImageRequestHandler *_handle = [[VNImageRequestHandler alloc] initWithCGImage:image.CGImage options:@{}];
+            NSError *error;
+            
+            VNDetectBarcodesRequest *requests = [[VNDetectBarcodesRequest alloc] initWithCompletionHandler:^(VNRequest * _Nonnull request, NSError * _Nullable error) {
+                if(!error) {
+                    NSArray<VNBarcodeObservation *> *vnObservers = ( NSArray<VNBarcodeObservation *> *)request.results;
+                    
+                    if(vnObservers.count == 0) {
+                        result(NULL);
+                        return;
+                    }
+                    NSLog(@"TEST message photo: %@", vnObservers.firstObject.payloadStringValue);
+                    result(vnObservers.firstObject.payloadStringValue);
+                }
+            }];
+            requests.symbologies = @[VNBarcodeSymbologyCode128, VNBarcodeSymbologyQR];
+            
+            [_handle performRequests:@[requests] error:&error];
+            
+        } else {
+            result(NULL);
+        }
     }
 }
 
